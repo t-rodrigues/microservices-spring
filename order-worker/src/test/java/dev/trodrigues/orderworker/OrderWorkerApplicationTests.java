a2a1a98883f53314134d247af9effb236941aa93
Thiago Rodrigues <thiagor_@live.com>
2022-02-04 20:16:14 -0300

    feat: create order-worker

---
+++ order-worker/src/test/java/dev/trodrigues/orderworker/OrderWorkerApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.trodrigues.orderworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
