0dece1c644d5eec22b144821504a5b9f3ee88414
Thiago Rodrigues <thiagor_@live.com>
2022-02-11 22:15:44 -0300

    test: ensure findById works as expected

--- ms-order/src/test/java/dev/trodrigues/order/services/impl/OrderServiceImplTest.java
+++ ms-order/src/test/java/dev/trodrigues/order/services/impl/OrderServiceImplTest.java
@@ -3,6 +3,7 @@ package dev.trodrigues.order.services.impl;
 import dev.trodrigues.order.domain.Order;
 import dev.trodrigues.order.domain.OrderMock;
 import dev.trodrigues.order.repositories.OrderRepository;
+import dev.trodrigues.order.services.exceptions.ObjectNotFoundException;
 import dev.trodrigues.order.services.rabbitmq.Producer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,10 +11,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,6 +45,29 @@ class OrderServiceImplTest {
 
         assertEquals(orderMock.getCep(), order.getCep());
         assertNotNull(order);
+    }
+
+    @Test
+    void shouldThrowWhenNonExistingId() {
+        var nonExistingId = 109L;
+
+        when(orderRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        assertThrows(ObjectNotFoundException.class, () -> orderServiceImpl.findById(nonExistingId));
+    }
+
+    @Test
+    void shouldReturnOrderWhenExistingId() {
+        var orderMock = OrderMock.creatOrder();
+        var existingId = orderMock.getId();
+
+        when(orderRepository.findById(existingId)).thenReturn(Optional.of(orderMock));
+
+        var order = orderServiceImpl.findById(existingId);
+
+        assertEquals(existingId, order.getId());
+        assertNotNull(order);
+        verify(orderRepository, times(1)).findById(existingId);
     }
 
 }
